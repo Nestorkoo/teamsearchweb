46907ae33a8f0c92014e68cf46242575b6ae9d81
Nestorkoo <nestorkodmytro@gmail.com>
2025-03-27 19:15:11 +0200

    Refresh gitignore

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 from decouple import config, RepositoryEnv, Config
 import os
 
+env_path = os.path.join(os.path.dirname(__file__), 'config',".env")
+config = Config(RepositoryEnv(env_path))
+
+
 DATABASE_URL = f"postgresql+asyncpg://{config('DB_USERNAME')}:{config("DB_PASSWORD")}5@{config('DB_HOST')}:{config('DB_PORT')}/{config("DB_NAME")}"
